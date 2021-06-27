3ad60965ec10220365a012fa74cf75a90b44fcd7
AliShiyyab <alishiyyab1998@gmail.com>
2021-06-27 00:43:49 -0700

    27/6 1 am

--- controllers/foodcontrole/food.js
+++ controllers/foodcontrole/food.js
@@ -1,6 +1,8 @@
 const { default: axios } = require("axios");
 
 const foodFunctions = {};
+const foodModel = require('../../models/foodschema.js')
+console.log(foodModel);
 
 // http://localhost:8000/food/getfood?foodName=<name>&cuision=<cuision>
 foodFunctions.getFood = (req,res) => {
@@ -37,7 +39,64 @@ foodFunctions.getFoodInfo = (req,res) => {
         res.status(400).send("Bad Request" , err)
     })
 }
+//get all dishes for user by email
+//http:localhost:8000/food/getFoodDishes?email=saadoundhirat93@gmail.com
+foodFunctions.getFoodDishes = (req,res) => {
+    let {email} = req.query;
+    console.log(foodModel);
+
+    foodModel.find({email:email} , (error,userData) =>{
+        console.log(userData[0].food);
+        if (error){
+            res.status(400).send('Bad Request TRY AGAIN');
+        }
+        else{
+            res.status(200).send(userData[0].food);
+        }
+    })
+}
 
 
+//get all dishes for user by email
+//http:localhost:8000/food/addFoodDishes?email=saadoundhirat93@gmail.com , foodObject
+
+//i will get email and food object
+foodFunctions.addFoodDishes = (req,res) => {
+    let {email} = req.query;
+    let {title , image , id} =req.body;
+    foodModel.find({email:email} , (error,userData) =>{
+        if (error){
+            res.status(400).send('Bad Request TRY AGAIN');
+        }
+        else{
+            userData[0].food.push({
+                id:id,
+                title:title,
+                image:image,
+            });
+            userData[0].save();
+            res.status(200).send(userData[0].food);
+        }
+    })
+}
+
+foodFunctions.deleteFoodDishes = (req,res) =>{
+    let {email} = req.query;
+    const id = Number(req.params.id);
+    console.log('ID : ' , id);
+    foodModel.find({email:email} , (error,userData) =>{
+        if (error){
+            res.status(400).send("Bad Way ! ");
+        }
+        let newFoodDishesFilter = userData[0].food.filter((item,idx)=>{
+            if (id !== idx){
+                return item;
+            }
+        });
+        userData[0].food = newFoodDishesFilter;
+        userData[0].save();
+        res.status(200).send(userData[0].food);
+    })
+}
 
 module.exports = foodFunctions;
--- routes/food.js
+++ routes/food.js
@@ -7,12 +7,23 @@ const router = express.Router();
 // import functions from food controller 
 const foodFunctions=require('../controllers/foodcontrole/food.js')
 
-
+// send req to hte api 
 router.get('/getFood', foodFunctions.getFood);
 router.get('/getFoodInfo', foodFunctions.getFoodInfo);
-// router.post('/' , fun);
+
+// send requiest to database
+// Returned data for which item was selecting (GET)
+router.get('/getFoodDishes' , foodFunctions.getFoodDishes);
+
+// To added new data of list.
+// add.
+router.post('/addFoodDishes' , foodFunctions.addFoodDishes);
+
+// To delete item which you are selected.
+// delete
+router.delete('/deleteFoodDishes' , foodFunctions.deleteFoodDishes);
 // router.delete('/' , fun);
 // router.put('/' , fun);
-// router.patch('/' , fun';
+// router.patch('/' , fun);
 
 module.exports = router;
